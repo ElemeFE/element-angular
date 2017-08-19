2d578f6f1c1e3b723ef328f90c991a4e5e27fb20
WittBulter <nanazuimeng123@gmail.com>
2017-08-20 01:58:49 +0800

    improve example

---
+++ ex/app/components/side/side.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'ex-side',
+  templateUrl: './side.component.html',
+  styleUrls: ['./side.component.scss'],
+})
+export class ExSideComponent implements OnInit {
+  
+  ngOnInit(): void {
+  }
+}
