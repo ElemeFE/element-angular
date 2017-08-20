fe2b753fb8b66eef7a76cbeb6c1ece9f14bb08d5
WittBulter <nanazuimeng123@gmail.com>
2017-08-20 13:27:48 +0800

    update style encapsulation

--- ex/app/app.component.ts
+++ ex/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewEncapsulation } from '@angular/core'
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
   title: string = 'Element Angular'
