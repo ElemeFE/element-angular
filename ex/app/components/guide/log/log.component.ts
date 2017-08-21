8bd795a7b2ad15d0d0e6675677d2900edb13b491
WittBulter <nanazuimeng123@gmail.com>
2017-08-22 01:12:05 +0800

    Ex: update style encapsulation

--- ex/app/components/guide/log/log.component.ts
+++ ex/app/components/guide/log/log.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewEncapsulation } from '@angular/core'
 
 @Component({
   selector: 'ex-log',
   templateUrl: './log.component.html',
   styleUrls: ['./log.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ExLogComponent implements OnInit {
   
