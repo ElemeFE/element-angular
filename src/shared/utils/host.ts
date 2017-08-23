c10a24186f606fec3a038b6e8b9a201b2ea8ec81
WittBulter <nanazuimeng123@gmail.com>
2017-08-23 18:00:12 +0800

    Util: fix parent is null

--- src/shared/utils/host.ts
+++ src/shared/utils/host.ts
@@ -1,6 +1,7 @@
 
 const removeNgTag = (nativeElement: HTMLElement): void => {
   const parentElement = nativeElement.parentElement
+  if (!parentElement || !parentElement.insertBefore) return
   while (nativeElement.firstChild) {
     parentElement.insertBefore(nativeElement.firstChild, nativeElement)
   }
