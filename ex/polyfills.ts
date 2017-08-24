e8e1d82b2dbb03ca613656dc99e49a792cba72ec
WittBulter <nanazuimeng123@gmail.com>
2017-08-24 14:39:27 +0800

    Ex: improve styles

--- ex/polyfills.ts
+++ ex/polyfills.ts
@@ -1,11 +1,3 @@
 import 'core-js/es6'
 import 'core-js/es7/reflect'
 import 'zone.js/dist/zone'
-
-// if (process.env.ENV === 'production') {
-//   // Production
-// } else {
-//   // Development and test
-//   Error['stackTraceLimit'] = Infinity
-//   require('zone.js/dist/long-stack-trace-zone')
-// }
