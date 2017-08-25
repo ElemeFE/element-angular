3bbbe7c3765602990bf1b53218c527c5de5b7594
WittBulter <nanazuimeng123@gmail.com>
2017-08-25 18:41:48 +0800

    Tag: improve template

--- src/tag/tag.ts
+++ src/tag/tag.ts
@@ -1,15 +1,41 @@
-import { Component, Input, ChangeDetectionStrategy} from '@angular/core'
+import { Component, Input, ChangeDetectionStrategy, OnInit, EventEmitter, Output } from '@angular/core'
+import { SafeStyle, DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'el-tag',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
+    <span [class]="'el-tag' + (type ? ' el-tag--' + type : '')"
+      [class.is-hit]="hit">
+      <ng-content></ng-content>
+      <i class="el-tag__close el-icon-close" *ngIf="closable"
+         (click)="closeHandle"></i>
+    </span>
   `,
 })
-export class ElTag {
+export class ElTag implements OnInit {
+  
+  @Input() type: string         // enum: primary/gray/success/warning/danger
+  @Input() closable: boolean = false
+  @Input() hit: boolean = false
+  @Input() color: string
+  @Input('close-transition') closeTransition: boolean = false
+  @Output() close: EventEmitter<any> = new EventEmitter<any>()
+  
+  private tagStyles: SafeStyle
   
   constructor(
+    private sanitizer: DomSanitizer,
   ) {
+  }
+  
+  closeHandle(): void {
+  
+  }
+  
+  ngOnInit(): void {
+    const styles: string = `backgroundColor: ${this.color}`
+    this.tagStyles = this.sanitizer.bypassSecurityTrustStyle(styles)
   }
   
 }
