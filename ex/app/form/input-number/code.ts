a0c7a52aa745ee1c95d5a2b2c8d456e637458572
WittBulter <nanazuimeng123@gmail.com>
2017-08-25 16:53:27 +0800

    Ex: improve input-number example code

--- ex/app/form/input-number/code.ts
+++ ex/app/form/input-number/code.ts
@@ -1,10 +1,33 @@
 export default [
 // 基础用法
 `
-<!--当设置 value 初始值属性后，会覆盖 placeholder-->
+
 <!--你可以通过 [(model)] 进行双向绑定-->
-<el-input [model]="input" placeholder="请输入内容"></el-input>
+<el-input-number [model]="5" [min]="1" [max]="10"></el-input-number>
 
 `,
 
+// 禁用
+`
+
+<el-input-number [model]="1" [disabled]="true"></el-input-number>
+
+`,
+
+// 步数
+`
+
+<el-input-number [model]="10" [step]="2"></el-input-number>
+
+`,
+
+// 尺寸
+`
+<el-input-number size="large" [model]="1"></el-input-number>
+
+<el-input-number [model]="3"></el-input-number>
+
+<el-input-number size="small" [model]="5"></el-input-number>
+
+`,
 ]
