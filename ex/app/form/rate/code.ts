d0f3e6777226c272058bc67e6a4e76aa26e77d8a
WittBulter <nanazuimeng123@gmail.com>
2017-08-29 00:38:46 +0800

    Ex: add rate doc

--- ex/app/form/rate/code.ts
+++ ex/app/form/rate/code.ts
@@ -1,13 +1,51 @@
 export default [
 // 基础用法
 `
-<!--你可以为 model 绑定布尔变量-->
-<!--或者通过绑定 (modelChange)=handle 来获得每次值改变的触发-->
-<el-switch [(model)]="value1" [on-text]="" [off-text]="">
-</el-switch>
-<el-switch [model]="true" [on-color]="'#13ce66'" [off-color]="'#ff4949'">
-</el-switch>
+<!--你可以为 model 绑定整数变量-->
+<!--通过绑定 (modelChange)=handle 来获得每次值改变的触发-->
+<div class="block">
+  <span class="demonstration">默认不区分颜色</span>
+  <el-rate [model]="4"></el-rate>
+</div>
+
+<div class="block">
+  <span class="demonstration">区分颜色</span>
+  <el-rate [model]="2"
+    [colors]="['#99A9BF', '#F7BA2A', '#FF9900']">
+  </el-rate>
+</div>
 
 `,
 
+// 辅助文字
+`
+<!--通过设置 [texts] 可以为每一个分值指定对应的辅助文字。-->
+<!-- [texts] 为一个数组，长度应等于最大值 [max] -->
+<el-rate [model]="3" [show-text]="true">
+</el-rate>
+
+`,
+
+// 其他icon
+`
+
+<el-rate [model]="4"
+  [icon-classes]="['el-icon-information', 'el-icon-information', 'el-icon-information']"
+  [void-icon-class]="'el-icon-information'"
+  [colors]="['#FF9900', '#FF9900', '#FF9900']">
+</el-rate>
+
+`,
+
+// 只读
+`
+
+<el-rate
+  [model]="3"
+  [disabled]="true"
+  [show-text]="true"
+  [text-color]="'#ff9900'">
+</el-rate>
+
+`,
 ]
