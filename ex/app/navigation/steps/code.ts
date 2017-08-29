6fc9769408a8c86c9b2e6e1a4ee508518a4ab5b6
WittBulter <nanazuimeng123@gmail.com>
2017-08-30 01:30:29 +0800

    Ex: add steps doc

--- ex/app/navigation/steps/code.ts
+++ ex/app/navigation/steps/code.ts
@@ -1,97 +1,59 @@
 export default [
 // 基础用法
 `
-<!--当设置 value 初始值属性后，会覆盖 placeholder-->
-<!--你可以通过 [(model)] 进行双向绑定-->
-<el-input [model]="input" placeholder="请输入内容"></el-input>
+<!--[active] 的值标识当前的进度 它是一个整数-->
+<div class="demo">
+  <el-steps space="100px" [active]="active" [finish-status]="'success'">
+    <el-step title="步骤 1"></el-step>
+    <el-step title="步骤 2"></el-step>
+    <el-step title="步骤 3"></el-step>
+  </el-steps>
+  <el-button (click)="handle()">next</el-button>
+</div>
+`,
+
+// 状态条
+`
+
+<el-steps space="100px" [active]="1" [finish-status]="'success'">
+  <el-step title="已完成"></el-step>
+  <el-step title="进行中"></el-step>
+  <el-step title="步骤 3"></el-step>
+</el-steps>
 
 `,
 
-// 禁用状态
+// 描述
 `
 
-<el-input placeholder="请输入内容"
-  [model]="input1"
-  [disabled]="true">
-</el-input>
+<el-steps space="200px" [active]="1">
+  <el-step title="步骤 1" description="这是一段很长很长很长的描述性文字"></el-step>
+  <el-step title="步骤 2" description="这是一段很长很长很长的描述性文字"></el-step>
+  <el-step title="步骤 3" description="这是一段很长很长很长的描述性文字"></el-step>
+</el-steps>
 
 `,
 
 // 图标
-
 `
 
-<!--你可以使用 (icon-click)=Function 来得到图标点击的事件-->
-<el-input placeholder="请选择日期"
-  icon="search"
-  [model]="input2">
-</el-input>
+<el-steps space="100px" [active]="1">
+  <el-step title="步骤 1" icon="edit"></el-step>
+  <el-step title="步骤 2" icon="upload"></el-step>
+  <el-step title="步骤 3" icon="picture"></el-step>
+</el-steps>
 
 `,
 
-// 文本域
+// 竖向
 `
-<el-input type="textarea"
-  [rows]="2"
-  placeholder="请输入内容"
-  [model]="textarea">
-</el-input>
-`,
 
-// 自动高度
-`
-<el-input type="textarea"
-  autosize
-  placeholder="固定高度文本域"
-  [model]="textarea2">
-</el-input>
+<el-steps space="100px" [active]="1" direction="vertical" [finish-status]="'success'">
+  <el-step title="步骤 1"></el-step>
+  <el-step title="步骤 2"></el-step>
+  <el-step title="步骤 3"></el-step>
+</el-steps>
 
-<div style="margin: 20px 0;"></div>
-
-<el-input type="textarea"
-  [autosize]="{ minRows: 2, maxRows: 4}"
-  placeholder="自动调整高度 { minRows: 2, maxRows: 4 }"
-  [model]="textarea3">
-</el-input>
-`,
-
-// 复合型
-`
-<div>
-  <el-input placeholder="请输入内容" [model]="input3">
-    <ng-template #prepend>
-      <span>Http://</span>
-    </ng-template>
-  </el-input>
-</div>
-
-<div style="margin-top: 15px;">
-  <el-input placeholder="请输入内容" [model]="input4">
-    <ng-template #append>
-      <span>.com</span>
-    </ng-template>
-  </el-input>
-</div>
-
-`,
-
-// 尺寸
-`
-<div class="demo-input-size">
-
-  <el-input size="large" placeholder="请输入内容" [model]="input6">
-  </el-input>
-  
-  <el-input placeholder="请输入内容" [model]="input7">
-  </el-input>
-  
-  <el-input size="small" placeholder="请输入内容" [model]="input8">
-  </el-input>
-  
-  <el-input size="mini" placeholder="请输入内容" [model]="input9">
-  </el-input>
-  
-</div>
 `,
 
 ]
