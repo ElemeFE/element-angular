d5cfd2601b39991bdc1371f5598aaacd71240081
WittBulter <nanazuimeng123@gmail.com>
2017-08-30 12:17:59 +0800

    Guide: add start steps

---
+++ ex/app/components/guide/start/code.ts
@@ -0,0 +1,74 @@
+export default [
+// 安装
+`
+# install cli
+npm install -g @angular/cli
+
+# init project
+ng new YOUR-PROJECT-NAME --style=scss
+cd YOUR-PROJECT-NAME
+
+# install element-angular
+npm i --save element-angular
+
+# run
+ng server
+
+`,
+
+// 引入
+
+`
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
+import { AppComponent } from './app.component'
+
+import { ElModule } from 'element-angular'
+// import style
+import 'element-theme-default'
+
+@NgModule({
+  declarations: [
+    ExAppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    ElModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [ExAppComponent],
+})
+export class AppModule {
+}
+
+// 不同组件与指令使用请参考具体章节
+`,
+
+// 单个组件引入
+`
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { MenuComponent } from './menu/menu.component'
+
+import { ElChildModules } from 'element-angular'
+
+@NgModule({
+  declarations: [
+    MenuComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ElChildModules.ElButton,          // just import button module
+  ],
+  exports: [ExGuideMainComponent],
+  providers: [],
+})
+export class MenuModule {
+}
+
+`,
+
+]
--- ex/app/components/guide/start/start.component.ts
+++ ex/app/components/guide/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import code from './code'
 
 @Component({
   selector: 'ex-start',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core'
 })
 export class ExStartComponent implements OnInit {
   
+  private code: string[] = code
   private page: any = {
     previous: { name: '安装', link: '/guide/install' },
     next: { name: '国际化', link: '/guide/i18n' },
