1396cc1310790f7e72cc876ed53d97ff09591089
WittBulter <nanazuimeng123@gmail.com>
2017-09-01 18:24:44 +0800

    Ex: create notify page

---
+++ ex/app/notice/notification/notification.component.ts
@@ -0,0 +1,38 @@
+import { Component, forwardRef, Inject, ViewEncapsulation } from '@angular/core'
+import { ElNotificationService } from '../../../../src/index'
+import code from './code'
+
+@Component({ selector: 'ex-notification-demo' })
+export class ExNotificationDemoComponent {
+  
+  constructor(
+    @Inject(forwardRef(() => ElNotificationService)) private notification: any,
+  ) {
+  }
+  
+  handle(): void {
+    console.log(this.notification)
+  }
+  
+}
+
+@Component({
+  selector: 'ex-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ExNotificationComponent {
+  
+  private code: string[] = code
+  private page: any = {
+    previous: { name: 'Message 消息', link: '/notice/message' },
+    next: { name: 'Loading 加载', link: '/notice/loading' },
+  }
+  private exClass: any = ExNotificationDemoComponent
+  
+  constructor(
+  ) {
+  }
+  
+}
