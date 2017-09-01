8fe17b229cccd762d0f7abee300fd0b56ed28338
WittBulter <nanazuimeng123@gmail.com>
2017-09-02 02:43:29 +0800

    Ex: add auto scroll top

--- ex/app/app.component.ts
+++ ex/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core'
+import { Router, NavigationEnd } from '@angular/router'
 
 @Component({
   selector: 'ex-app',
@@ -7,8 +8,25 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core'
   encapsulation: ViewEncapsulation.None,
 })
 export class ExAppComponent implements OnInit {
+  
   title: string = 'Element Angular'
+  private lastRouteUrl: string[] = []
+  
+  constructor(
+    private router: Router,
+  ) {
+  }
   
   ngOnInit(): void {
+    this.router.events.subscribe((ev) => {
+      const len = this.lastRouteUrl.length
+      if (ev instanceof NavigationEnd) {
+        this.lastRouteUrl.push(ev.url)
+        if (len > 1 && ev.url === this.lastRouteUrl[len - 2]) {
+          return
+        }
+        window.scrollTo(0, 0)
+      }
+    })
   }
 }
