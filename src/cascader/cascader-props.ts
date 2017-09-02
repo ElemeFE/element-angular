41944cfcea4e279ffc230908bfa490347edacf43
WittBulter <nanazuimeng123@gmail.com>
2017-09-03 03:14:13 +0800

    Doc: add cascader doc

--- src/cascader/cascader-props.ts
+++ src/cascader/cascader-props.ts
@@ -12,7 +12,6 @@ export class ElCascaderPoprs {
   // input props
   @Input() disabled: boolean = false
   @Input() size: string
-  @Input('popper-class') popperClass: string
   @Input() placeholder: string = '请选择'
   
   // data
