601d3ad15e6bb1ec0e3801128e1465eb7ac96b0d
WittBulter <nanazuimeng123@gmail.com>
2017-09-03 17:47:14 +0800

    Badge: improve template

--- ex/app/data/badge/code.ts
+++ ex/app/data/badge/code.ts
@@ -2,12 +2,50 @@ export default [
 // 基础用法
 `
 
-<el-tag>标签一</el-tag>
-<el-tag type="gray">标签二</el-tag>
-<el-tag type="primary">标签三</el-tag>
-<el-tag type="success">标签四</el-tag>
-<el-tag type="warning">标签五</el-tag>
-<el-tag type="danger">标签六</el-tag>
+<el-badge [model]="12" class="item">
+  <el-button size="small">评论</el-button>
+</el-badge>
+
+<el-badge [model]="3" class="item">
+  <el-button size="small">回复</el-button>
+</el-badge>
+
+`,
+
+// 最大值
+`
+
+<el-badge [model]="200" [max]="99" class="item">
+  <el-button size="small">评论</el-button>
+</el-badge>
+
+<el-badge [model]="100" [max]="10" class="item">
+  <el-button size="small">回复</el-button>
+</el-badge>
+
+`,
+
+// 自定义内容
+`
+
+<el-badge model="new" class="item">
+  <el-button size="small">评论</el-button>
+</el-badge>
+
+<el-badge model="hot" class="item">
+  <el-button size="small">回复</el-button>
+</el-badge>
+
+`,
+
+// 小红点
+`
+
+<el-badge [is-dot]="true" class="item">数据查询</el-badge>
+
+<el-badge [is-dot]="true" class="item">
+  <el-button class="share-button" icon="share" type="primary"></el-button>
+</el-badge>
 
 `,
 ]
--- src/badge/badge.ts
+++ src/badge/badge.ts
@@ -4,14 +4,29 @@ import { Component, Input, ChangeDetectionStrategy} from '@angular/core'
   selector: 'el-badge',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
+    <div class="el-badge">
+      <ng-content></ng-content>
+      <sup *ngIf="!hidden && (makeContent() || isDot)"
+        class="el-badge__content"
+        [class.is-fixed]="true" [class.is-dot]="isDot">
+        {{makeContent()}}
+      </sup>
+    </div>
   `,
 })
 export class ElBadge {
   
-  @Input('name') iconName: string
+  @Input() model: string | number
+  @Input() max: number                // only model is number
+  @Input() hidden: boolean = false
+  @Input('is-dot') isDot: boolean = false
   
-  constructor(
-  ) {
+  makeContent(): string | number {
+    if (this.isDot) return ''
+    if (typeof this.model === 'number') {
+      return this.max < this.model ? `${this.max}+` : this.model;
+    }
+    return this.model
   }
   
 }
