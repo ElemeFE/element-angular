c52fef6ab10ada666826bf1ad629fafd6e5adf6d
WittBulter <nanazuimeng123@gmail.com>
2017-09-03 21:57:18 +0800

    Ex: improve drop page

---
+++ ex/app/navigation/dropdown/dropdown.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core'
+import code from './code'
+
+@Component({
+  selector: 'ex-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ExDropdownComponent implements OnInit {
+  
+  private exClass: new () => {} = class {
+    
+    private data: any[] = [{
+      value: 'huangjingao',
+      label: '黄金糕',
+    }, {
+      value: 'shizitou',
+      label: '狮子头',
+    }, {
+      value: 'luosifen',
+      label: '螺蛳粉',
+    }]
+    private data2: any[] = [{
+      value: 'huangjingao',
+      label: '黄金糕',
+    }, {
+      value: 'shizitou',
+      label: '狮子头',
+      disabled: true,
+    }, {
+      value: 'luosifen',
+      label: '螺蛳粉',
+    }]
+    private data3: any[] = [{
+      value: 'huangjingao',
+      label: '黄金糕',
+    }, {
+      value: 'shizitou',
+      label: '狮子头',
+      divided: true,
+    }, {
+      value: 'luosifen',
+      label: '螺蛳粉',
+    }]
+    
+    handle(): void {
+    }
+  }
+  private code: string[] = code
+  private page: any = {
+    previous: { name: 'Steps 步骤条', link: '/nav/steps' },
+    next: { name: 'Tooltip 文字提示', link: '/others/tooltip' },
+  }
+  
+  
+  ngOnInit(): void {
+  }
+}
