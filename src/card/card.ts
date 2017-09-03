c4d1a54fef0017938357d00451f9a2952c6da616
WittBulter <nanazuimeng123@gmail.com>
2017-09-03 18:27:09 +0800

    Card: init component

---
+++ src/card/card.ts
@@ -0,0 +1,39 @@
+import { Component, Input, ChangeDetectionStrategy, ContentChild, TemplateRef } from '@angular/core'
+import { SafeStyle, DomSanitizer } from '@angular/platform-browser'
+
+@Component({
+  selector: 'el-card',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <div [class]="'el-card ' + nativeClass">
+      <div class="el-card__header" *ngIf="header || headerStr">
+        <ng-container *ngIf="header">
+          <ng-template [ngTemplateOutlet]="header">
+          </ng-template>
+        </ng-container>
+        <ng-container *ngIf="!header">
+          {{headerStr}}
+        </ng-container>
+      </div>
+      <div class="el-card__body" [style]="makeSafebodyStyle()">
+        <ng-content></ng-content>
+      </div>
+    </div>
+  `,
+})
+export class ElCard {
+  
+  @ContentChild('header') header: TemplateRef<any>
+  @Input('header') headerStr: string
+  @Input('body-style') bodyStyle: string = ''
+  
+  constructor(
+    private sanitizer: DomSanitizer,
+  ) {
+  }
+  
+  makeSafebodyStyle(): SafeStyle {
+    return this.sanitizer.bypassSecurityTrustStyle(this.bodyStyle)
+  }
+
+}
