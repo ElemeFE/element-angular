75bb7c50152f245312c49ffdf714765d06ddae4a
WittBulter <nanazuimeng123@gmail.com>
2017-09-03 22:43:39 +0800

    Cascader: add outside click event

--- src/cascader/cascader.menu.ts
+++ src/cascader/cascader.menu.ts
@@ -7,14 +7,15 @@ import { dropAnimation } from '../shared/animation'
   template: `
     <div class="el-cascader-menus"
       style="z-index: 2007; position: absolute; top: 100%; left: 0;"
-      [@dropAnimation]="root.menuVisible">
+      [@dropAnimation]="root.menuVisible"
+      (click)="clickHandle($event)">
       <ul class="el-cascader-menu" *ngFor="let menuItem of root.steps; let step = index">
         <li *ngFor="let listItem of menuItem; let i = index"
           class="el-cascader-menu__item"
           [class.el-cascader-menu__item--extensible]="listItem.children"
           [class.is-active]="listItem.active"
           [class.is-disabled]="listItem.disabled"
-          (click)="root.selectHandle(step, i)">
+          (click)="root.selectHandle($event, step, i)">
           {{listItem.label}}
         </li>
       </ul>
@@ -27,5 +28,9 @@ export class ElCascaderMenu {
   constructor(
     @Optional() private root: ElCascader,
   ) {
+  }
+  
+  clickHandle(event: Event): void {
+    event.stopPropagation()
   }
 }
