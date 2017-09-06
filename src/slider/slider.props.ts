dbd4f3c51966d33ca7ff887389565e34516403b9
WittBulter <nanazuimeng123@gmail.com>
2017-09-07 01:44:21 +0800

    Doc: add slider doc

--- src/slider/slider.props.ts
+++ src/slider/slider.props.ts
@@ -5,12 +5,10 @@ export class ElSliderProps {
   @Input() min: number = 0
   @Input() max: number = 100
   @Input() disabled: boolean = false
-  @Input() step: number = 1
   
-  @Input('show-input') showInput: boolean = false
-  @Input('show-input-controls') showInputControls: boolean = true
+  // @Input('show-input') showInput: boolean = false
+  // @Input('show-input-controls') showInputControls: boolean = true
   @Input('show-tooltip') showTooltip: boolean = true
-  @Input('show-stops') showStops: boolean = false
   @Input('format-tooltip') formatTooltip: Function
   
   @Input() vertical: boolean = false
