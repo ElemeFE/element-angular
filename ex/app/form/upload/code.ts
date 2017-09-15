a82b92bb02229e700e0e1ef33b1877d8b85266bf
WittBulter <nanazuimeng123@gmail.com>
2017-09-15 12:15:54 +0800

    Ex: add upload example

--- ex/app/form/upload/code.ts
+++ ex/app/form/upload/code.ts
@@ -3,7 +3,7 @@ export default [
 `
 <el-upload action="http://jsonplaceholder.typicode.com/posts"
   [file-list]="fileList"
-  (upload-filter)="limit500($event)">
+  [upload-filter]="limit500.bind(this)">
   <ng-template #trigger>
     <el-button size="small" type="primary">点击上传2</el-button>
   </ng-template>
@@ -15,17 +15,62 @@ export default [
 <script type="text">
 // in component, file is native file
 
-limit500(event: any): void {
-  const file: File = event.file.raw
+limit500(file: File): boolean {
   if (file.size > 500000) {
-    this.message.warn('文件超过了 500 kb.')
-    event.reject()
-  } else {
-    event.next()
+    this.message.info('文件超过了 500 kb.')
+    return false
   }
+  return true
 }
 
 </script>
+`,
+
+`
+
+<el-upload class="avatar-uploader"
+  action="https://jsonplaceholder.typicode.com/posts/"
+  [show-file-list]="false"
+  (success)="successHandle($event)"
+  (error)="errorHandle($event)">
+  <ng-template #trigger>
+    <img *ngIf="imageUrl" [src]="imageUrl" class="avatar">
+    <i *ngIf="!imageUrl" class="el-icon-plus avatar-uploader-icon"></i>
+  </ng-template>
+</el-upload>
+
+<script type="text">
+// in component, file is native file
+
+successHandle(file: any): void {
+  this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file.raw))
+  this.message.info('文件上传成功')
+}
+
+errorHandle(err: any): void {
+  this.message.error('文件上传失败:' + err)
+}
+
+</script>
+
+`,
+
+// 照片墙
+`
+
+<el-upload action="https://jsonplaceholder.typicode.com/posts/"
+  list-type="picture-card"
+  (preview)="previewHandle($event)"
+  (remove)="removeHandle($event)">
+  <ng-template #trigger>
+    <i class="el-icon-plus"></i>
+  </ng-template>
+</el-upload>
+
+<el-dialog [(visible)]="showDialog" size="tiny">
+  <img width="100%" [src]="dialogImageUrl" alt="">
+</el-dialog>
+
 `,
 
 ]
--- ex/app/form/upload/upload.component.ts
+++ ex/app/form/upload/upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, forwardRef, Inject, ViewEncapsulation } from '@angular/core'
 import { ElMessageService } from '../../../../src/index'
+import { SafeUrl, DomSanitizer } from '@angular/platform-browser'
 import code from './code'
 
 @Component({ selector: 'ex-upload-demo' })
@@ -12,22 +13,45 @@ export class ExUploadDemoComponent {
     {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99b' +
     'ce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
     }]
+  private imageUrl: SafeUrl
+  private dialogImageUrl: SafeUrl
+  private showDialog: boolean = false
   
   constructor(
-    @Inject(forwardRef(() => ElMessageService)) private message: any,
+    @Inject(forwardRef(() => ElMessageService)) private message: ElMessageService,
+    @Inject(forwardRef(() => DomSanitizer)) private sanitizer: DomSanitizer,
   ) {
   }
   
-  limit500(event: any): void {
-    const file: File = event.file.raw
+  limit500(file: File): boolean {
     if (file.size > 500000) {
       this.message.info('文件超过了 500 kb.')
-      event.reject()
-    } else {
-      event.next()
+      return false
     }
+    return true
   }
   
+  successHandle(event: any): void {
+    const file: File = event.commonFile.raw
+    this.imageUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file))
+    this.message.info('文件上传成功')
+  }
+  
+  errorHandle(event: any): void {
+    const { error } = event
+    this.message.error('文件上传失败:' + error)
+  }
+  
+  previewHandle(commonFile: any): void {
+    const file: File = commonFile.raw
+    this.dialogImageUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file))
+    console.log(this.dialogImageUrl)
+    this.showDialog = true
+  }
+  
+  removeHandle(commonFile: any): void {
+    this.message.info('文件已移除')
+  }
 }
 
 @Component({
