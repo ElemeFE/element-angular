06855138f097131e7def3a92df6a1e3666943169
WittBulter <nanazuimeng123@gmail.com>
2017-10-02 22:00:15 +0800

    Build: add messages check

--- build/bin/upload.locale.js
+++ build/bin/upload.locale.js
@@ -5,31 +5,34 @@ const readFile = util.promisify(fs.readFile)
 const request = require('request-promise-native')
 const { JSDOM } = require("jsdom")
 
+const isChecked = units => {
+  const ids = units.map(unit => unit.unit_id)
+  return new Set(ids).size === ids.length
+}
 const findToken = async() => {
   const tokenFilePath = `${__dirname}/../../.git/authentication_token.txt`
-  if (!await exists(tokenFilePath)) return
+  if (!await exists(tokenFilePath)) return ''
   return await readFile(tokenFilePath, 'utf-8')
 }
 
 ;(async () => {
-  const localSourcePath = `${__dirname}/../../ex/messages.xlf`
-  const msgFileIsExist = await exists(localSourcePath)
-  if (!msgFileIsExist) {
-    console.log('making messages file...')
-    require('child_process').spawn('npm', ['run', 'i18n'])
-  }
+  console.log('making messages file...')
+  require('child_process').spawn('npm', ['run', 'i18n'])
   
+  const localSourcePath = `${__dirname}/../../ex/messages.xlf`
   const msgFile = await readFile(localSourcePath, 'utf-8')
   const { document } = new JSDOM(msgFile, {contentType: "text/xml",}).window
   const units = Array.from(document.querySelectorAll('trans-unit')).map(v => {
     return {
       source: v.querySelector('source').textContent,
       unit_id: v.getAttribute('id'),
-      target: 'rr',
       line_number: +v.querySelector('context[context-type="linenumber"]').textContent,
       source_file: v.querySelector('context[context-type="sourcefile"]').textContent,
     }
   })
+  if (!isChecked(units)) {
+    return console.log('failed. the locale serial number of the duplicate')
+  }
   const token = await findToken()
   request({
     uri: 'http://127.0.0.1:3000/elements',
@@ -41,7 +44,7 @@ const findToken = async() => {
     },
     json: { units: units },
   })
-  .then(() => console.log('updated locale keys!'))
+  .then(() => console.log('upload locale keys!'))
   .catch(e => {
     if (e.statusCode === 403) {
       return console.log('Authentication failed, try login.')
