18c8aa5a67d38a34ac696065a1803dfaf0c79d9c
WittBulter <nanazuimeng123@gmail.com>
2017-10-13 00:03:35 +0800

    Ex: improve container page

--- ex/app/basic/container/code.ts
+++ ex/app/basic/container/code.ts
@@ -1,94 +1,54 @@
 export default [
 // 基础的栅格布局
 `
-<div el-row>
-  <div el-col span="24"><div class="grid-content bg-purple-dark"></div></div>
-</div>
-<div el-row>
-  <div el-col span="12"><div class="grid-content bg-purple"></div></div>
-  <div el-col span="12"><div class="grid-content bg-purple-light"></div></div>
-</div>
-<div el-row>
-  <div el-col span="8"><div class="grid-content bg-purple"></div></div>
-  <div el-col span="8"><div class="grid-content bg-purple-light"></div></div>
-  <div el-col span="8"><div class="grid-content bg-purple"></div></div>
-</div>
-<div el-row>
-  <div el-col span="6"><div class="grid-content bg-purple"></div></div>
-  <div el-col span="6"><div class="grid-content bg-purple-light"></div></div>
-  <div el-col span="6"><div class="grid-content bg-purple"></div></div>
-  <div el-col span="6"><div class="grid-content bg-purple-light"></div></div>
-</div>
-<div el-row>
-  <div el-col span="4"><div class="grid-content bg-purple"></div></div>
-  <div el-col span="4"><div class="grid-content bg-purple-light"></div></div>
-  <div el-col span="4"><div class="grid-content bg-purple"></div></div>
-  <div el-col span="4"><div class="grid-content bg-purple-light"></div></div>
-  <div el-col span="4"><div class="grid-content bg-purple"></div></div>
-  <div el-col span="4"><div class="grid-content bg-purple-light"></div></div>
-</div>
+
+<el-container class="demo-box">
+  <el-header>Header</el-header>
+  <el-main>Main</el-main>
+</el-container>
+
+<el-container class="demo-box">
+  <el-header>Header</el-header>
+  <el-main>Main</el-main>
+  <el-footer>Footer</el-footer>
+</el-container>
+
+
+<el-container class="demo-box">
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+
+<el-container class="demo-box">
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
+</el-container>
+
+<el-container class="demo-box">
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+
+<el-container class="demo-box">
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</el-container>
+
 `,
-// 分栏之间存在间隔
-`
-<div el-row gutter="20">
-  <div el-col [span]="6">
-    <div class="grid-content bg-purple"></div>
-  </div>
-  <div el-col span="6">
-    <div class="grid-content bg-purple"></div>
-  </div>
-  <div el-col span="6">
-    <div class="grid-content bg-purple"></div>
-  </div>
-  <div el-col span="6">
-    <div class="grid-content bg-purple"></div>
-  </div>
-</div>
-`,
-// 复杂的混合布局
-`
-<div el-row gutter="20">
-  <div el-col [span]="16"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="8"><div class="grid-content bg-purple"></div></div>
-</div>
-<div el-row :gutter="20">
-  <div el-col [span]="8"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="8"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="4"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="4"><div class="grid-content bg-purple"></div></div>
-</div>
-<div el-row :gutter="20">
-  <div el-col [span]="4"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="16"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="4"><div class="grid-content bg-purple"></div></div>
-</div>
-`,
-// 对齐方式
-`
-<div el-row type="flex" class="row-bg">
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple-light"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-</div>
-<div el-row type="flex" class="row-bg" justify="center">
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple-light"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-</div>
-<div el-row type="flex" class="row-bg" justify="end">
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple-light"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-</div>
-<div el-row type="flex" class="row-bg" justify="space-between">
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple-light"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-</div>
-<div el-row type="flex" class="row-bg" justify="space-around">
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple-light"></div></div>
-  <div el-col [span]="6"><div class="grid-content bg-purple"></div></div>
-</div>
-`
 ]
