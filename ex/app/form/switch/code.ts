f5851d78c33265320895a44d726b8329c1a1ec94
WittBulter <nanazuimeng123@gmail.com>
2017-10-31 18:38:23 +0800

    Switch: upgrade to 2.0

--- ex/app/form/switch/code.ts
+++ ex/app/form/switch/code.ts
@@ -3,9 +3,9 @@ export default [
 `
 <!--你可以为 model 绑定布尔变量-->
 <!--或者通过绑定 (modelChange)=handle 来获得每次值改变的触发-->
-<el-switch [(model)]="value1" [on-text]="" [off-text]="">
+<el-switch [(model)]="value1">
 </el-switch>
-<el-switch [model]="true" [on-color]="'#13ce66'" [off-color]="'#ff4949'">
+<el-switch [model]="true" [active-color]="'#13ce66'" [inactive-color]="'#ff4949'">
 </el-switch>
 
 `,
@@ -13,10 +13,9 @@ export default [
 // 有禁用选项
 `
 
-<el-switch [(model)]="value1" [on-text]="" [off-text]=""
-  [disabled]="true">
+<el-switch [(model)]="value1"[disabled]="true">
 </el-switch>
-<el-switch [(model)]="value2" [on-color]="'#13ce66'" [off-color]="'#ff4949'"
+<el-switch [(model)]="value2" [active-color]="'#13ce66'" [inactive-color]="'#ff4949'"
   [disabled]="true">
 </el-switch>
 
@@ -25,8 +24,18 @@ export default [
 // 自定义图标
 `
 
-<el-switch [model]="true" [on-text]="" [off-text]=""
-  [on-icon-class]="'el-icon-circle-check'" [off-icon-class]="'el-icon-circle-cross'">
+<el-switch [model]="true" [active-icon-class]="'el-icon-circle-check'"
+  [inactive-icon-class]="'el-icon-circle-cross'">
+</el-switch>
+
+
+`,
+
+// 自定义图标
+`
+
+<el-switch [model]="true" active-text="移动"
+  inactive-text="联通">
 </el-switch>
 
 
