ac7a63fd7aa26fe1c6de7c3698b93b694a946aaa
WittBulter <nanazuimeng123@gmail.com>
2017-11-01 12:18:29 +0800

    Ex: append simple mode

--- ex/app/navigation/steps/code.ts
+++ ex/app/navigation/steps/code.ts
@@ -3,19 +3,19 @@ export default [
 `
 <!--[active] 的值标识当前的进度 它是一个整数-->
 <div class="demo">
-  <el-steps space="100px" [active]="active" [finish-status]="'success'">
+  <el-steps [active]="active" [finish-status]="'success'">
     <el-step title="步骤 1"></el-step>
     <el-step title="步骤 2"></el-step>
     <el-step title="步骤 3"></el-step>
   </el-steps>
-  <el-button (click)="handle()">next</el-button>
 </div>
+<el-button (click)="handle()">next</el-button>
 `,
 
 // 状态条
 `
 
-<el-steps space="100px" [active]="1" [finish-status]="'success'">
+<el-steps space="150px" [active]="1" [finish-status]="'success'">
   <el-step title="已完成"></el-step>
   <el-step title="进行中"></el-step>
   <el-step title="步骤 3"></el-step>
@@ -26,7 +26,7 @@ export default [
 // 描述
 `
 
-<el-steps space="200px" [active]="1">
+<el-steps  [active]="1" [align-center]="true">
   <el-step title="步骤 1" description="这是一段很长很长很长的描述性文字"></el-step>
   <el-step title="步骤 2" description="这是一段很长很长很长的描述性文字"></el-step>
   <el-step title="步骤 3" description="这是一段很长很长很长的描述性文字"></el-step>
@@ -37,7 +37,7 @@ export default [
 // 图标
 `
 
-<el-steps space="100px" [active]="1">
+<el-steps [active]="1">
   <el-step title="步骤 1" icon="edit"></el-step>
   <el-step title="步骤 2" icon="upload"></el-step>
   <el-step title="步骤 3" icon="picture"></el-step>
@@ -46,9 +46,20 @@ export default [
 `,
 
 // 竖向
+  `
+
+<el-steps space="120px" [active]="1" direction="vertical" [finish-status]="'success'">
+  <el-step title="步骤 1"></el-step>
+  <el-step title="步骤 2"></el-step>
+  <el-step title="步骤 3"></el-step>
+</el-steps>
+
+`,
+
+// 简洁风格
 `
 
-<el-steps space="100px" [active]="1" direction="vertical" [finish-status]="'success'">
+<el-steps [active]="1" [simple]="true">
   <el-step title="步骤 1"></el-step>
   <el-step title="步骤 2"></el-step>
   <el-step title="步骤 3"></el-step>
