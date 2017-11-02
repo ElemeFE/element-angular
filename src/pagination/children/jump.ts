8a91247d5a734af6ede4567e41321b78eea4787e
WittBulter <nanazuimeng123@gmail.com>
2017-11-02 15:32:39 +0800

    Jump: fix jump component style

--- src/pagination/children/jump.ts
+++ src/pagination/children/jump.ts
@@ -5,12 +5,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
   template: `
     <span class="el-pagination__jump">
       前往
-    <input class="el-el-input__inner" type="number"
-      #input
-      [min]="1" [max]="max"
-      [ngModel]="model"
-      (keyup.enter)="changeHandle(input.value)"
-      (blur)="changeHandle(input.value)"/>
+    <div class="el-input el-pagination__editor is-in-pagination">
+      <input class="el-input__inner" type="number" #input
+        [min]="1" [max]="max" [ngModel]="model"
+        (keyup.enter)="changeHandle(input.value)" (blur)="changeHandle(input.value)"/>
+    </div>
       页
     </span>
   `,
