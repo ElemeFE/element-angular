70de09252d7f858eb4726da32a0b4691f6d64862
WittBulter <nanazuimeng123@gmail.com>
2017-12-03 18:44:51 +0800

    Ex: fix float style

--- ex/app/form/rate/code.ts
+++ ex/app/form/rate/code.ts
@@ -3,16 +3,18 @@ export default [
 `
 <!--你可以为 model 绑定整数变量-->
 <!--通过绑定 (modelChange)=handle 来获得每次值改变的触发-->
-<div class="block">
-  <span class="demonstration">默认不区分颜色</span>
-  <el-rate [model]="4"></el-rate>
-</div>
-
-<div class="block">
-  <span class="demonstration">区分颜色</span>
-  <el-rate [model]="2"
-    [colors]="['#99A9BF', '#F7BA2A', '#FF9900']">
-  </el-rate>
+<div class="rate-demo">
+  <div class="block">
+    <span class="demonstration">默认不区分颜色</span>
+    <el-rate [model]="4"></el-rate>
+  </div>
+  
+  <div class="block">
+    <span class="demonstration">区分颜色</span>
+    <el-rate [model]="2"
+      [colors]="['#99A9BF', '#F7BA2A', '#FF9900']">
+    </el-rate>
+  </div>
 </div>
 `,
 
