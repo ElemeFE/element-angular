25541a7537271e064eae70ed699657d7c402443c
WittBulter <nanazuimeng123@gmail.com>
2018-01-07 02:55:07 +0800

    feat(tree): improve example page

--- ex/app/data/tree/code.ts
+++ ex/app/data/tree/code.ts
@@ -1,6 +1,7 @@
 export default [
 // 基础用法
 `
-
+<el-tree [model]="data">
+</el-tree>
 `,
 ]
--- ex/app/data/tree/tree.component.ts
+++ ex/app/data/tree/tree.component.ts
@@ -1,6 +1,25 @@
 import { Component, ViewEncapsulation } from '@angular/core'
 import code from './code'
 
+class DemoClass {
+  data: any = [{
+    label: '一级 1',
+    children: [{
+      label: '二级 1-1',
+      children: [{
+        label: '三级 1-1-1',
+      }]
+    }]
+  }, {
+    label: '一级 2',
+    children: [{
+      label: '二级 2-1'
+    }]
+  }, {
+    label: '一级 3',
+  }]
+}
+
 @Component({
   selector: 'ex-tree',
   templateUrl: './tree.component.html',
@@ -9,14 +28,11 @@ import code from './code'
 })
 export class ExTreeComponent {
   
-  private code: string[] = code
-  private tags: string[] = ['123', '234234', '0011']
-  private page: any = {
+  exClass: any = DemoClass
+  code: string[] = code
+  page: any = {
     previous: { name: 'Table 表格', link: '/data/table' },
     next: { name: 'Loading 加载', link: '/notice/loading' },
-  }
-  
-  handle(): void {
   }
   
 }
