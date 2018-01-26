a5b490f0920469095af7e5db187ad07d86713b7a
WittBulter <nanazuimeng123@gmail.com>
2018-01-26 15:55:43 +0800

    feat(loading): append loading-top attr to directive

--- src/loading/loading.directive.ts
+++ src/loading/loading.directive.ts
@@ -1,22 +1,27 @@
 import {
   Input, Directive, OnInit, ElementRef, Renderer2,
-  OnChanges, SimpleChanges,
 } from '@angular/core'
 import { DocumentWrapper, WindowWrapper } from '../shared/services'
 
 @Directive({
   selector: '[el-loading]',
 })
-export class ElLoadingDirective implements OnInit, OnChanges {
+export class ElLoadingDirective implements OnInit {
   
   @Input() text: string
   @Input() lock: boolean = false        // ban scroll on loading active
   @Input('custom-class') customClass: string
   @Input('full-screen') fullScreen: boolean = false
-  @Input('el-loading') showLoading: boolean = false
+  @Input('el-loading-top') elLoadingTop: string = '50%'
+  @Input('el-loading') set showLoading(val: boolean) {
+    this.visible = val
+    if (!this.cacheElement) return
+    this.cacheElement.innerHTML = this.makeHtml()
+  }
   
   cacheElement: HTMLElement
   cacheOverflow: string
+  private visible: boolean = false
   
   constructor(
     private el: ElementRef,
@@ -33,11 +38,11 @@ export class ElLoadingDirective implements OnInit, OnChanges {
   }
   
   makeHtml(): string {
-    this.lock && this.toggleLock(this.showLoading)
+    this.lock && this.toggleLock(this.visible)
     return `
       <div class="el-loading-mask ${this.customClass} ${this.fullScreen ? ' is-fullscreen' : ''}"
-        style="display: ${this.showLoading ? 'block': 'none'}">
-        <div class="el-loading-spinner">
+        style="display: ${this.visible ? 'block': 'none'}">
+        <div class="el-loading-spinner" style="top: ${this.elLoadingTop}">
           <svg class="circular" viewBox="25 25 50 50">
             <circle class="path" cx="50" cy="50" r="20" fill="none"/>
           </svg>
@@ -62,12 +67,6 @@ export class ElLoadingDirective implements OnInit, OnChanges {
       this.renderer.setStyle(this.el.nativeElement, 'position', 'relative')
     }
     this.renderer.appendChild(parentElement, this.cacheElement)
-  }
-  
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.showLoading && this.cacheElement) {
-      this.cacheElement.innerHTML = this.makeHtml()
-    }
   }
   
 }
