f8edb8346224b8c2434b85aedc453f1db4953086
WittBulter <nanazuimeng123@gmail.com>
2018-04-21 16:05:22 +0800

    feat(collapse): auto chenge tracking

--- src/collapse/collapse-item.ts
+++ src/collapse/collapse-item.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, Input, Optional, TemplateRef } from '@angular/core'
+import { Component, ContentChild, Input, OnInit, Optional, TemplateRef } from '@angular/core'
 import { ElCollapse, ModelValue } from './collapse'
 import { dropAnimation } from '../shared/animation'
 
@@ -7,7 +7,7 @@ import { dropAnimation } from '../shared/animation'
   animations: [dropAnimation],
   styles: [`.el-collapse-item-fix-border:last-child { margin-bottom: 0; }`],
   template: `
-    <div class="el-collapse-item el-collapse-item-fix-border" [class.is-active]="isActive()">
+    <div class="el-collapse-item el-collapse-item-fix-border" [class.is-active]="isActive">
       <div role="tab" [attr.aria-expanded]="isActive">
         <div class="el-collapse-item__header" (click)="clickHandle()" role="button">
           <i class="el-collapse-item__arrow el-icon-arrow-right"></i>
@@ -19,27 +19,28 @@ import { dropAnimation } from '../shared/animation'
         </div>
       </div>
       
-      <div class="el-collapse-item__wrap" [ngStyle]="{'border-width': isActive() ? '1px' : '0'}">
-        <div class="el-collapse-item__content" [@dropAnimation]="isActive()">
+      <div class="el-collapse-item__wrap" [ngStyle]="{'border-width': isActive ? '1px' : '0'}">
+        <div class="el-collapse-item__content" [@dropAnimation]="isActive">
           <ng-content></ng-content>
         </div>
       </div>
     </div>
   `,
 })
-export class ElCollapseItem {
+export class ElCollapseItem implements OnInit {
   
   @ContentChild('label') labelTmp: TemplateRef<any>
   @Input() label: string
   @Input() value: ModelValue | null = null
+  isActive: boolean = false
   
   constructor(
     @Optional() private root: ElCollapse,
   ) {
   }
   
-  isActive(): boolean {
-    return this.root.modelValue.some(val => val === this.value)
+  updateActiveStatus(): void {
+    this.isActive = this.root.modelValue.some(val => val === this.value)
   }
   
   clickHandle(): void {
@@ -47,6 +48,11 @@ export class ElCollapseItem {
       this.value = Math.random().toString(16).substr(2, 8)
     }
     this.root.updateModel(this.value)
+  }
+  
+  ngOnInit(): void {
+    const updateHandle: Function = () => this.updateActiveStatus()
+    this.root.subscriber.push(updateHandle)
   }
   
 }
