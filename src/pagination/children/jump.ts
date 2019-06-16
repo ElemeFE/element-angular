32a09d4b7c8b05ba1d4b72c56b8b6724ebf55575
Sascha Braun <sascha.braun245@gmail.com>
2019-06-16 19:55:29 +0200

    feat: pagination uses locales

--- src/pagination/children/jump.ts
+++ src/pagination/children/jump.ts
@@ -1,17 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { ElLocalesService, ElPaginationLocales } from '../../locales';
 
 @Component({
   selector: 'el-pagination-jump',
   template: `
     <span class="el-pagination__jump">
-      前往
+      {{ locales.goto }}
     <div class="el-input el-pagination__editor is-in-pagination">
       <input class="el-input__inner" type="number" #input
         autocomplete="off" rows="2"
         [min]="1" [max]="max" [ngModel]="model"
         (keyup.enter)="changeHandle(input.value)" (blur)="changeHandle(input.value)"/>
     </div>
-      页
+      {{ locales.pageClassifier }}
     </span>
   `,
 })
@@ -21,10 +22,21 @@ export class ElPaginationJump {
   @Input() max: number
   @Output() next: EventEmitter<number> = new EventEmitter<number>()
   
+  locales: ElPaginationLocales;
+
   static nextPageNumber(page: number, max: number): number {
     if (page <= 1) return 1
     if (page >= max) return max
     return page
+  }
+
+  constructor(private readonly elLocales: ElLocalesService) {
+  }
+
+  ngOnInit(): void {
+    this.elLocales.resources$.subscribe(locales => {
+      this.locales = locales.el.pagination;
+    })
   }
   
   changeHandle(nextValue: number): void {
